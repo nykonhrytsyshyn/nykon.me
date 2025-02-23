e6614643ede83fa5d9114429426ae60d0abe2df8
Nykon Hrytsyshyn <admin@nykon.me>
2025-02-23 18:34:19 +0200

    test

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,27 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import clsx from "clsx";
 import { fontSans } from "@configs/fonts";
-import { ReactElement } from "react";
+import React, { ReactElement } from "react";
 import Script from "next/script";
 
 export default function Document(): ReactElement {
   return (
     <Html suppressHydrationWarning lang="en">
-      <Head />
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+        try {
+          if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.add('dark')
+          } else {
+            document.documentElement.classList.remove('dark')
+          }
+        } catch (_) {}
+      `,
+          }}
+        />
+      </Head>
       <body
         className={clsx(
           "min-h-screen font-sans antialiased",
